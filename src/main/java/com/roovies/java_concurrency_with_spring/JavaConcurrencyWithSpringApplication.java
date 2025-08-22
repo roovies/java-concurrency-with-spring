5b93f95094211cee7fc925c2d2d428065fe76ca9
roovies <roovies77@gmail.com>
2025-08-22 09:36:32 +0900

    init: 초기 프로젝트

---
+++ src/main/java/com/roovies/java_concurrency_with_spring/JavaConcurrencyWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.roovies.java_concurrency_with_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaConcurrencyWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaConcurrencyWithSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roovies/java_concurrency_with_spring/JavaConcurrencyWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roovies.java_concurrency_with_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaConcurrencyWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
