97002e2ff54782deef9e72ce4bae1c70b3c60c41
roovies <roovies77@gmail.com>
2025-09-04 18:02:52 +0900

    feat(race-condition): Spring Retry를 통한 낙관적 락 재시도 로직 구현

--- src/main/java/com/roovies/java_concurrency_with_spring/JavaConcurrencyWithSpringApplication.java
+++ src/main/java/com/roovies/java_concurrency_with_spring/JavaConcurrencyWithSpringApplication.java
@@ -2,12 +2,13 @@ package com.roovies.java_concurrency_with_spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableRetry // Spring Retry 활성화
 public class JavaConcurrencyWithSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaConcurrencyWithSpringApplication.class, args);
 	}
-
 }
